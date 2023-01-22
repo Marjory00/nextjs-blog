9c615ea5e88dc2316089e0c067ea9310846f15cf
Marjory Marquez <samaterra@hotmail.com>
2023-01-22 03:29:54 -0500

    blog

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Marjoy Marquez';
+const name = 'Marjory Marquez';
 export const siteTitle = 'Next.js Blog Website';
 
 export default function Layout({ children, home }) {
